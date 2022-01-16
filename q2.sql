5d178bd3ea4722f38b539daf79c696217f84dda1
ClementTsai <90400408+ClementTsai@users.noreply.github.com>
2022-01-15 19:18:38 -0500

    Update q2.sql
    Updated comments

--- q2.sql
+++ q2.sql
@@ -1,47 +1,46 @@
-Question 2: For this question you’ll need to use SQL. Follow this link to access the data set required for the challenge. Please use queries to answer the following questions. Paste your queries along with your final numerical answers below.
+--Question 2: For this question you’ll need to use SQL. Follow this link to access the data set required for the challenge. Please use queries to answer the following questions. Paste your queries along with your final numerical answers below.
 
-How many orders were shipped by Speedy Express in total?
+--How many orders were shipped by Speedy Express in total?
 
-– Find number of orders shipped by Speedy Express
+--Find number of orders shipped by Speedy Express
 SELECT COUNT(OrderID) AS 'Amt Shipped by Speedy Express'
-– Join relevant tables
+–-Join relevant tables
 FROM Orders o
 JOIN Shippers s
 ON o.ShipperID = s.ShipperID
-– On the condition that the Shipper is Speedy Express
+–-On the condition that the Shipper is Speedy Express
 WHERE s.ShipperName LIKE 'Speedy Express';
 
-54 Orders were shipped by Speedy Express
+--54 Orders were shipped by Speedy Express
 
 
 
 
-What is the last name of the employee with the most orders?
-— Find the the employee with the MOST CountTotals
+--What is the last name of the employee with the most orders?
+—-Find the the employee with the MOST CountTotals
 SELECT LastName AS "Last Name", MAX(CountTotal) AS "Amount Sold"
-— Finding the collective amount of ALL orders for all employees
+—-Finding the collective amount of ALL orders for all employees
 FROM(SELECT Count(o.OrderID) AS CountTotal, e.LastName
 FROM Orders o
 JOIN Employees e
 ON O.EmployeeID = e.EmployeeID
 GROUP BY e.EmployeeID)
 
-The last name of the employee with the most orders was Peacock with 40 orders
+--The last name of the employee with the most orders was Peacock with 40 orders
 
-What product was ordered the most by customers in Germany?
-
-— Finding the most ordered product
+--What product was ordered the most by customers in Germany?
+—-Finding the most ordered product
 SELECT ProductName AS 'Product Name', MAX(Total) AS 'Amount Ordered'
-— Finding the quantity of ALL ordered products
+—-Finding the quantity of ALL ordered products
 FROM(SELECT SUM(Quantity) AS Total, ProductName
-— Joining relevant tables
+—-Joining relevant tables
 FROM(SELECT c.CustomerID, c.Country, o.OrderID, p.ProductID, od.Quantity, p.ProductName
 	FROM Customers c
 	JOIN Orders o ON o.CustomerID = c.CustomerID
 	JOIN OrderDetails od ON od.OrderID = o.OrderID
 	JOIN Products p ON p.ProductID = od.ProductID
-— On the condition that the orders are from Germany
+—-On the condition that the orders are from Germany
 WHERE c.Country LIKE 'Germany')
 GROUP BY ProductID)
 
-The most ordered product by customers in Germany was the Boston Crab Meat with 160 orders
+--The most ordered product by customers in Germany was the Boston Crab Meat with 160 orders
